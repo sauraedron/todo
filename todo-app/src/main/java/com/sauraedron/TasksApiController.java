7fd1ca93c085e1e68e2369b035e9c55709980f96
Saurabh Wankhade <sauraedron@gmail.com>
2024-10-03 15:43:54 +0530

    changes after generator cli use

--- todo-app/src/main/java/com/sauraedron/TasksApiController.java
+++ todo-app/src/main/java/com/sauraedron/TasksApiController.java
@@ -1,18 +1,18 @@
 package com.sauraedron;
 
-import com.sauraedron.wire_objects.CreateTask;
-import com.sauraedron.wire_objects.MasterTask;
+import com.sauraedron.wire_models.CreateTask;
+import com.sauraedron.wire_models.MasterTask;
+import jakarta.annotation.Generated;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
 
-import javax.annotation.Generated;
 import java.util.List;
 import java.util.Optional;
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-10-03T10:58:59.766050500+05:30[Asia/Calcutta]", comments = "Generator version: 7.8.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-10-03T12:37:08.841886200+05:30[Asia/Calcutta]", comments = "Generator version: 7.8.0")
 @Controller
 @RequestMapping("${openapi.tODO.base-path:}")
 public class TasksApiController implements TasksApi {
